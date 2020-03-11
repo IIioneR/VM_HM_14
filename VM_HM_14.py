aac638d091492b3d9e30ae5766eb6ded31ee5ded
IIioneR <IIioneR@ukr.net>
2020-03-11 21:37:25 +0300

    second_task

--- VM_HM_14.py
+++ VM_HM_14.py
@@ -1,16 +1,5 @@
-import random
-
-def random_number():
-    return random.randint(-100, 100)
-
-print(random_number())
-
-
 def is_simple(n):
     d = 2
     while n % d != 0:
         d += 1
     return d == n
-
-
-print(is_simple(5))
